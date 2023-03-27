74a9a6844a57abea49dd2152281e4a55fc6c5519
James Martin <jamesmartin@Jamess-MBP.lan>
2023-03-27 18:50:08 -0400

    created fs.writefile for README.md

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+
+# 
+
+# Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Description:
+![License](https://img.shields.io/badge/License--blue.svg "License Badge")
+undefined
+## Installation:
+undefined
+## Usage:
+undefined
+## Contributing:
+undefined
+## Test: 
+undefined
+## Credits:
+undefined
+## License:
+For more information about the License, click on the link below.
+- [License](https://opensource.org/licenses/)
+##  Questions:
+For questions about the Generator you can go to my 
+Github page at the following link:
+- [Github Profile](https://github.com/undefined)
+For additional questions please reach out to me via email at: .
--- Develop/index.js
+++ Develop/index.js
@@ -2,13 +2,13 @@ let fs = require('fs')
 let inquirer = require('inquirer')
 let util = require('util')
 // const generate = function ({name, location, email, github, linkedin, projectName})
-
+let writeFile = util.promisify(fs.writeFile)
 
 let questions = () => {
    return inquirer.prompt([
      {
         type: 'input',
-        name: 'name',
+        name: 'title',
         message: 'Please type your first and last name.'
 
      },
@@ -55,8 +55,54 @@ let questions = () => {
      }
     ])
 };
-questions()
+let markdown = (response) => {
+   return `
+# ${response.title}
 
+# Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Description:
+![License](https://img.shields.io/badge/License-${response.license}-blue.svg "License Badge")
+${response.description}
+## Installation:
+${response.installation}
+## Usage:
+${response.usage}
+## Contributing:
+${response.contributing}
+## Test: 
+${response.test}
+## Credits:
+${response.credits}
+## License:
+For more information about the License, click on the link below.
+- [License](https://opensource.org/licenses/${response.license})
+##  Questions:
+For questions about the Generator you can go to my 
+Github page at the following link:
+- [Github Profile](https://github.com/${response.username})
+For additional questions please reach out to me via email at: ${response.email}.
+`
+}
+
+async function init(){
+   try{
+      const response = await questions()
+      const rm = markdown(response)
+
+      await writeFile('README.md', rm)
+   }catch(err) {console.log(err)}
+}
+init()
 // // TODO: Create a function to write README file
 // function writeToFile(README, data) {
 //    fs.writeFile('README.md', data, err => {
